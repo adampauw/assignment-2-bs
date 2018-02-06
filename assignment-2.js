419e57c8f6af06ea4803925078132285385eca8d
adampauw <adampauw@gmail.com>
2018-02-05 21:09:17 -0800

    upload to github

---
+++ assignment-2.js
@@ -0,0 +1,28 @@
+/* Challenge 1 */
+
+for(var i = 1; i < 8; i++) {
+    console.log("#".repeat(i));
+    }
+
+/* Here I set the parameters for the loop then had it console.log the Pound sign,
+ after I used the .repeat() method with a count equal to our variable (i). */
+
+
+ /* Challenge 2 */
+
+ var num = x ;
+ 
+ if ( num % 2 === 0) {
+     console.log('Even Number');
+ }
+ else if  ( num % 2 === 1){
+     console.log('Odd Number');
+ }
+ else {
+    console.log('not a number');
+  }
+ 
+  /*
+Here I created a variable called 'num', if the number is evenly divisable by 2 it will log 'even number'
+if it has a remainder of 0 it will then log 'odd number'
+lastly if you input a 'string' the console will log 'not a number'  */
